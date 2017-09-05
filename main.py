4675a54e18ae21ecd8165f22b6d6ad90e772744f
tbauer516 <tcbauer516@gmail.com>
2017-09-04 17:30:33 -0700

    finished off functionality. can downvote to disable

--- main.py
+++ main.py
@@ -3,13 +3,11 @@
 # Created by /u/tbauer516
 # License: MIT License
 
-####################
-# import all libraries here
-# includes other setup of widely used variables
-####################
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
+from random import randint
 
+import os
 import praw
 import time
 import re
@@ -17,107 +15,134 @@ import requests
 import bs4
 import sys
 
-# local user account the file will run on
-locUser = 'tbauer516'
-# location of file where id's of already visited comments are maintained
-path = '/home/' + locUser + '/TakesCreditBot/commented.txt'
+filename = 'commented.txt'
+masterAccount = 'tbauer516'
 
-# regex used to find the comments we are looking for
-commentRegex = '[gG][oO]{2}[dD]\s[bB][oO][tT][.]{0,3}'
-# timeout between gathering comment chunks
 apiTimeout = 60
+numComments = 250
+subredditNames = ['test']
+# 'ShowerThoughts', 'photoshopbattles', 'InternetIsBeautiful', 'mildlyinteresting'
 
-# responses stored in a list
-responses = ['Thank you, glad my effort can be recognized.']
+myCommentKarma = None
+commentKarmaThreshold = 0
 
-####################
-# used to authenticate with Reddit, then return an instance used
-# to interact with the reddit API
-####################
+responses = [
+    'Thank you, glad my effort can be appreciated.',
+    'Thanks, just doing my best to be helpful.',
+    'Thanks, I put a lot of hard work into this.'
+]
+
+userBotRegex = '(?i)bot'
+commentPraiseRegex = '(?i)good\sbot[.]{0,3}'
+
+previousComments = set()
+
+path = os.path.join(os.path.dirname(os.path.realpath(__file__)), filename)
+
 def authenticate():
     print('Authenticating...\n')
     reddit = praw.Reddit('TakesCreditBot', user_agent = 'web:takes-credit-bot:v0.1 (by /u/tbauer516)')
     print('Authenticated as {}\n'.format(reddit.user.me()))
     return reddit
 
-####################
-# used to determine if the comment passed is of interest to us
-####################
+
+def hasDownvotesToDisable(reddit):
+    myCommentKarma = int(reddit.user.me().comment_karma)
+    return myCommentKarma < commentKarmaThreshold
+
+
+def sendExitPM(reddit, user):
+    print('Sending exit PM to ' + user)
+    reddit.redditor(user).message('Bot Has Been Disabled',
+        'TakesCreditBot has reached a large number of '
+        + ' downvotes and has thus been shut down until '
+        + ' human action takes place to re-enable it.')
+
+
 def isCommentMatch(comment):
-    match = re.findall(commentRegex, comment.body)
+    match = re.findall(commentPraiseRegex, comment.body)
     return match
 
-####################
-# used to determine if the user passed is a bot
-####################
+
 def isUserBot(user):
     # TODO: add more criteria for determining a bot
-    if (re.search('[bB][oO][tT]', user.name)):
+    if (re.search(userBotRegex, user.name)):
         return True
+    # TODO: remove for when we actually want to filter on who is and is not a bot
+    return True
     return False
 
-####################
-# parse a single comment
-####################
-def parseComment(comment):
+
+def isParentCommentUserBot(comment):
     parent = comment.parent()
-    if (isUserBot(parent.author)):
-        print('parent is a bot')
-        print(comment.author.name + ' praised a bot with comment ID: ' + comment.id)
+    return isUserBot(parent.author)
 
-####################
-# adds an entry to the tracking file of comments that have
-# been previously commented on
-####################
-def updateCommentTrackerFile(comment):
-    commentedFileObj = open(path,'a+')
 
-    if comment.id not in commentedFileObj.read().splitlines():
-        print('comment is new...posting reply\n')
-        # TODO: uncomment below lines
-        # comment.reply(responses[0])
-        # commentedFileObj.write(comment.id + '\n')
+def isCommentNew(comment):
+    return not comment.id in previousComments
 
-    commentedFileObj.close()
-    time.sleep(10)
 
-####################
-# function to match, read and track a single comment
-####################
+def replyToComment(comment):
+    print('replying to: ' + comment.author.name)
+    comment.reply(responses[randint(0, len(responses) - 1)])
+    previousComments.add(comment.id)
+
+
+def saveCommentsToFile():
+    with open(path, 'w+') as commentedFileObj:
+        for item in previousComments:
+            commentedFileObj.write(item)
+
+
+def loadCommentsFromFile():
+    with open(path, 'r') as commentedFileObj:
+        for item in commentedFileObj.read().splitlines():
+            previousComments.clear()
+            previousComments.add(item)
+
+
 def operateOnSingleComment(comment):
     match = isCommentMatch(comment)
     if match:
-        parseComment(comment)
-        updateCommentTrackerFile(comment)
+        print('\n')
+        print(comment.author.name + ' praised a bot with comment ID: ' + comment.id)
 
-####################
-# single loop instance that runs the bot
-####################
+        if (not isCommentNew(comment)):
+            return
+        print('comment is new')
+
+        if (not isParentCommentUserBot(comment)):
+            return
+        print('parent is a bot')
+        
+        replyToComment(comment)
+
+
 def runBot(reddit):
-    print('Getting 250 comments...\n')
-    
-    for comment in reddit.subreddit('test').comments(limit = 250):
+    print('Getting {} comments...\n'.format(numComments))
+    for comment in reddit.subreddit(', '.join(subredditNames)).comments(limit = numComments):
         operateOnSingleComment(comment)
 
-    print('Waiting {} seconds...\n'.format(apiTimeout))
-    time.sleep(apiTimeout)
 
-####################
-# function that can be called to run a test on a single comment
-# without running a loop
-####################
 def runTest(reddit):
     print('running test')
     for comment in reddit.submission(id = '6vf2av').comments.list():
         operateOnSingleComment(comment)
 
-####################
-# loop that calls the main loop above
-####################
+
 def main():
     reddit = authenticate()
+    loadCommentsFromFile()
     while True:
         runBot(reddit)
+        saveCommentsToFile()
+        if (hasDownvotesToDisable(reddit)):
+            sendExitPM(reddit, masterAccount)
+            break
+
+        print('Waiting {} seconds...\n'.format(apiTimeout))
+        time.sleep(apiTimeout)
+
 
 def test():
     reddit = authenticate()
